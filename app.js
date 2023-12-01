1164945c49f5faa2e12c8348e7846b7356ef51e5
Eric Winebrenner <ejwinebrenner@gmail.com>
2023-12-01 15:24:40 -0500

    updated setup

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = 4000;
 //* IMPORTS
+const routeController = require('./controllers/routes.controller');
 
 //* MIDDLEWARE
 app.use(express.json());
 
 //* CONTROLLERS
+app.use('/routes', routeController);
 
 
 //* RUNNING
